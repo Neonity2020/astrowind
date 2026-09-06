64debe56588c6f050d97ee84176d056df036c6a6
prototypa <prototypearea.dev@gmail.com>
2026-09-06 11:07:57 -0400

    chore(footer): credit link points to arthelokyo.com

--- src/navigation.ts
+++ src/navigation.ts
@@ -163,6 +163,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://arthelokyo.com"> Arthelokyo</a> · All rights reserved.
   `,
 };
